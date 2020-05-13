61f1c3e257e47a1aa042923ac2b403e217d56048
听着情歌流泪 <develop@lingtal.com>
2020-05-13 16:58:13 +0800

    bug(eslint-config-sealui): 修改`no-return-await`规则
    修改`no-return-await`规则

--- packages/eslint-config-sealui/lib/base.js
+++ packages/eslint-config-sealui/lib/base.js
@@ -622,7 +622,7 @@ module.exports = {
     /**
      * 禁止在 return 语句里使用 await
      */
-    'no-return-await': 'off',
+    'no-return-await': 'error',
     /**
      * 禁止出现 location.href = 'javascript:void(0)';
      * @reason 有些场景下还是需要用到这个
