b807dacf2e03e189bf07aeb3e3c0f7cbac54a171
听着情歌流泪 <develop@lingtal.com>
2020-05-14 16:05:19 +0800

    fix(.cz-config.js): 修改错误的描述
    修改错误的描述

--- .cz-config.js
+++ .cz-config.js
@@ -3,36 +3,36 @@ module.exports = {
   types: [
     {
       value: 'feat',
-      name: '✨ - feat:     新功能'
+      name: '✨ - feat:     新功能',
     },
     {
-      value: 'bug',
-      name: '🐞 - bug:      bug修复'
+      value: 'fix',
+      name: '🐞 - fix:      bug修复',
     },
     {
       value: 'refactor',
-      name: '🛠  - refactor: 重构代码'
+      name: '🛠  - refactor: 重构代码',
     },
     {
       value: 'docs',
-      name: '📚 - docs:     文档改动'
+      name: '📚 - docs:     文档改动',
     },
     {
       value: 'test',
-      name: '🏁 - test:     添加缺失的测试, 重构测试, 不包括生产代码变动'
+      name: '🏁 - test:     添加缺失的测试, 重构测试, 不包括生产代码变动',
     },
     {
       value: 'chore',
-      name: '🗯  - chore:    更新构建任务等; 不包括生产代码变动'
+      name: '🗯  - chore:    更新构建任务等; 不包括生产代码变动',
     },
     {
       value: 'style',
-      name: '💅 - style:    格式化, 缺失分号等; 不包括生产代码变动'
+      name: '💅 - style:    格式化, 缺失分号等; 不包括生产代码变动',
     },
     {
       value: 'revert',
-      name: '⏪ - revert:   Revert to a commit'
-    }
+      name: '⏪ - revert:   Revert to a commit',
+    },
   ],
   scopes: [],
   allowCustomScopes: true,
@@ -43,7 +43,7 @@ module.exports = {
     subject: '写一个简短的变化描述 <subject>\n',
     body: '更长的变更描述(可选)。使用“|”换行 <body>\n',
     footer: '列出此更改所关闭的任何问题(可选). 例如：#1, #2 <footer>\n',
-    confirmCommit: '您确定要继续执行上面的提交吗?'
+    confirmCommit: '您确定要继续执行上面的提交吗?',
   },
-  allowBreakingChanges: ['feat', 'fix']
+  allowBreakingChanges: ['feat', 'fix'],
 }
